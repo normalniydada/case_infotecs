527edae3d5f7b3e60665716d1ae04f062ad2263f
juggernaut <normalniydadawade@gmail.com>
2025-07-21 00:18:10 +0700

    feat(app):wallet service fix (int/float) -> decimal

--- internal/application/wallet/wallet.go
+++ internal/application/wallet/wallet.go
@@ -10,7 +10,7 @@ import (
 	"github.com/normalniydada/case_infotecs/internal/domain/models"
 	"github.com/normalniydada/case_infotecs/internal/domain/repository"
 	"github.com/normalniydada/case_infotecs/internal/domain/service"
-	"github.com/normalniydada/case_infotecs/internal/pkg/converter"
+	"github.com/shopspring/decimal"
 )
 
 type walletService struct {
@@ -21,30 +21,31 @@ func NewWalletService(walletRepo repository.WalletRepository) service.WalletServ
 	return &walletService{walletRepo: walletRepo}
 }
 
-func (s *walletService) Balance(ctx context.Context, address string) (float64, error) {
+func (s *walletService) Balance(ctx context.Context, address string) (decimal.Decimal, error) {
 	wallet, err := s.walletRepo.Wallet(ctx, address)
 	if err != nil {
-		return 0, fmt.Errorf("ошибка при получении баланса: %w", err)
+		return decimal.NewFromInt(0), fmt.Errorf("error while getting balance: %w", err)
 	}
 
-	return converter.ConvertIntToFloat(wallet.Balance), nil
+	return wallet.Balance, nil
 }
 
-func (s *walletService) TransferMoney(ctx context.Context, from, to string, amount float64) error {
+func (s *walletService) TransferMoney(ctx context.Context, from, to string, amount decimal.Decimal) error {
 	if from == to {
 		return er.ErrSameWalletTransfer
 	}
-	if amount < 0 {
+
+	if amount.LessThanOrEqual(decimal.NewFromInt(0)) {
 		return er.ErrInvalidAmount
 	}
 
-	return s.walletRepo.Transfer(ctx, from, to, converter.ConvertFloatToInt(amount))
+	return s.walletRepo.Transfer(ctx, from, to, amount)
 }
 
-func (s *walletService) CreateWallet(ctx context.Context, balance float64) error {
+func (s *walletService) CreateWallet(ctx context.Context, balance decimal.Decimal) error {
 	wallet := models.Wallet{
 		Address: generateWalletAddress(),
-		Balance: converter.ConvertFloatToInt(balance),
+		Balance: balance,
 	}
 
 	return s.walletRepo.CreateWallet(ctx, &wallet)
